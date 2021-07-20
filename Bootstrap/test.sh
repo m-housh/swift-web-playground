629553ce8ba612fb57a218e15f62fb48f0fe8db6
Michael Housh <mhoush@houshhomeenergy.com>
2021-07-20 12:44:22 -0400

    Added CI workflows

--- Bootstrap/test.sh
+++ Bootstrap/test.sh
@@ -4,7 +4,7 @@ apt-get install -y \
   cmake libpq-dev libssl-dev libz-dev openssl postgresql sudo libsqlite3-dev
 service postgresql start
 sudo -u postgres createuser --superuser playground
-sudo -u postgres psql "ALTER USER playground PASSWORD 'playground';"
+sudo -u postgres psql -c "ALTER USER playground PASSWORD 'playground';"
 sudo -u postgres createdb --owner playground playground_test
 sudo chown "$USER":"$USER" Bootstrap/test.sh
 swift test || exit $?
