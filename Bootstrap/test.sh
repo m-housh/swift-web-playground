6834a28949c58f2d824430731962343608d99749
Michael Housh <mhoush@houshhomeenergy.com>
2021-07-20 12:27:09 -0400

    Fix ubuntu tests.

--- Bootstrap/test.sh
+++ Bootstrap/test.sh
@@ -2,10 +2,8 @@
 apt-get --fix-missing update
 apt-get install -y \
   cmake libpq-dev libssl-dev libz-dev openssl postgresql sudo
-  
 service postgresql start
 sudo -u postgres createuser --superuser playground
-sudo -u postgres psql "ALTER USER playground PASSWORD 'playground'"
+sudo -u postgres psql "ALTER USER playground PASSWORD 'playground';"
 sudo -u postgres createdb --owner playground playground_test
-
 swift test || exit $?
